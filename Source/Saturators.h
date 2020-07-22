c09b11286758349ceec0428684ddeca8e59479fc
Bennet Leff <bennetleff@gmail.com>
2020-07-22 13:13:48 -0500

    Clean up Saturators
    starting to optimize file

--- Source/Saturators.h
+++ Source/Saturators.h
@@ -14,7 +14,7 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include <math.h>
 
-static const int LUTSize = 400, LUTHalfSize = LUTSize/2, LUTMaxIndex = LUTSize-1;
+constexpr static const int LUTSize = 400, LUTHalfSize = LUTSize/2, LUTMaxIndex = LUTSize-1;
 static float aSinHlookupTable[LUTSize];
 
 enum SatType
@@ -31,7 +31,6 @@ using SatFunc = std::function<float(float)>;
 class Saturators
 {
 public:
-
     static SatFunc getSaturator(SatType type)
     {
         if (type == SatType::hard)
@@ -45,13 +44,12 @@ public:
 
         if (type == SatType::ahypsin)
             return [](float x) { return aSinhClip(x); };
-            //return [](float x) { return aSinhClipLUT(x); };
 
         // None
         return [](float x) { return x; };
     }
 
-    static inline float hardClip(float x)
+    constexpr static inline float hardClip(float x)
     {
         if (x > 1.0f)
             return 1.0f;
@@ -62,7 +60,7 @@ public:
         return x;
     }
 
-    static inline float softClip(float x)
+    constexpr static inline float softClip(float x)
     {
         if (x > 1.0f)
             return 2.0f / 3.0f;
@@ -81,34 +79,24 @@ public:
     static inline float aSinhClip(float x)
     {
         return asinhf(x);
-        
     }    
     
     static inline float aSinhClipLUT(float x)
     {
         float pi = MathConstants<float>::pi;
         int inputToIndex = jmax(0, static_cast<int>(LUTHalfSize + floor(LUTHalfSize * x / pi)));
-        //x=pi -> inputToIndex = 400 (out of range)
-        //x=-pi -> inputToIndex = 0
-        //x=0 ->inputToIndex= 200
+
         int LUTIndex = jmin(LUTMaxIndex, inputToIndex);
         return aSinHlookupTable[LUTIndex];
-        
     }
 
     //fills table with asinh values 
-    static void fillLUT() {
+    constexpr static void fillLUT() {
         int minVal = LUTSize / -2;
-        //int maxVal = LUTSize / 2 - 1;
 
         for (int i = 0; i < LUTSize; i++) {
             float piRangeFromIndex = ((minVal + i) * MathConstants<float>::pi) / (LUTSize / 2);
             aSinHlookupTable[i] = asinhf(piRangeFromIndex);
-            //DBG(i<<": " << aSinHlookupTable[i]);
         }
-
     }
-
-
-
 };
